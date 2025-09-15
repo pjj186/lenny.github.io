cda0a49d7cd904fc58841c443df9a9af3e59ea97
박종재 <66498240+pjj186@users.noreply.github.com>
2025-09-15 09:57:58 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -242,7 +242,7 @@ export default BlogIndex
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="모든 포스트" />
+export const Head = () => <Seo title="Home" />
 
 export const pageQuery = graphql`
   {
