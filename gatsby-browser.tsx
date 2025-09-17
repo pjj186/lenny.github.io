1563aa15008016f83d3fdcd2f7d0eed9ec86c68a
박종재 <66498240+pjj186@users.noreply.github.com>
2025-09-17 18:16:57 +0900

    Update gatsby-browser.tsx

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,22 +1,22 @@
 // custom typefaces
-import "@fontsource-variable/montserrat"
-import "@fontsource/merriweather"
+import "@fontsource-variable/montserrat";
+import "@fontsource/merriweather";
 // normalize CSS across browsers
 // import "./src/normalize.css"
 
 // custom CSS styles
-import "./src/style.css"
+import "./src/style.css";
 
 // Highlighting for code blocks
-import "prismjs/themes/prism.css"
+import "prismjs/themes/prism-tomorrow.css";
 
-import React from "react"
-import { ThemeProvider } from "./src/components/theme-provider"
+import React from "react";
+import { ThemeProvider } from "./src/components/theme-provider";
 
 export const wrapRootElement = ({ element }) => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       {element}
     </ThemeProvider>
-  )
-}
+  );
+};
